5a0cb28bf0a21fc393fdc8664958af2e11c0a949
Aaron Carneiro <euaaron@outlook.com>
2021-10-16 20:18:25 -0300

    feat: different screen options acording to OS

--- electron/electron.ts
+++ electron/electron.ts
@@ -5,6 +5,11 @@ import { environment } from '../src/environments/environment';
 
 const ipc = ipcMain;
 let win: BrowserWindow;
+let windowOption : {
+  transparent: boolean,
+  frame: boolean,
+  titleBarStyle: 'hidden' | 'hiddenInset' | 'customButtonsOnHover' | 'default',
+};
 
 function createWindow() {
   const { screen } = require('electron');
@@ -18,15 +23,39 @@ function createWindow() {
     height: display.height * 0.8,
   };
 
+  if(process.platform === 'win32') {
+    windowOption =  {
+      transparent: false,
+      titleBarStyle: 'hidden',
+      frame: true
+    }
+  } else if(process.platform === 'darwin') {
+    windowOption =  {
+      transparent: false,
+      titleBarStyle: 'hiddenInset',
+      frame: true
+    }
+  } else {
+    windowOption =  {
+      transparent: true,
+      titleBarStyle: 'hidden',
+      frame: false
+    }
+  }
+
+  const { transparent, frame, titleBarStyle } = windowOption;
+
   // Create the browser window.
   win = new BrowserWindow({
     width: windowPos.width,
     height: windowPos.height,
     minWidth: 360,
     minHeight: 240,
-    transparent: false,
     roundedCorners: true,
-    titleBarStyle: 'hidden',
+    titleBarStyle,
+    transparent,
+    frame,
+    backgroundColor: '#282a36',
     webPreferences: {
       webviewTag: true,
       nodeIntegration: true,
@@ -44,7 +73,10 @@ function createWindow() {
   });
 
   if (!environment.production) {
-    win.loadURL('http://localhost:4200');
+    win.loadURL('http://localhost:4200').catch((err) => {
+      console.log(err);
+      win.close();
+    });
   } else {
     win.loadURL(
       url.format({
